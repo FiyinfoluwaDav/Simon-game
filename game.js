cae7839bf6479712be641dee9e5bba0fa9bb1c07
Osokoya Fiyinfoluwa <osokoyafiyinfoluwa@gmail.com>
2025-10-08 08:10:03 +0100

    Fix typo in buttonColors variable name and update HTML class attribute

--- game.js
+++ game.js
@@ -1,10 +1,39 @@
-var buttonColors = ["red", "blue", "green", "yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
-function nextSequence(){
-    randomNumber = Math.floor(Math.random()*buttonColors.length);
-    var randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor);
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  // Flash animation (simulate fadeIn/fadeOut)
+  var button = document.getElementById(randomChosenColour);
+
+  button.style.opacity = "0";
+  setTimeout(function () {
+    button.style.opacity = "1";
+  }, 100);
+  setTimeout(function () {
+    button.style.opacity = "0";
+  }, 200);
+  setTimeout(function () {
+    button.style.opacity = "1";
+  }, 300);
+
+  // Play corresponding sound
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+
+  //Check when button is pressed
+  button.addEventListener('click',function(){
+    userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+  })
 }
 
-document.getElementById(randomChosenColor);
+// Start game when any key is pressed
+document.addEventListener("keydown", function() {
+  nextSequence();
+});
